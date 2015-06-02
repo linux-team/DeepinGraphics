a529ad584b16ef5768a10aa689f99fdfe3458e31
deepinzhangshuang <13617166@163.com>
2015-06-02 17:39:02 +0800

    add

--- DQstring.h
+++ DQstring.h
@@ -1,58 +1,59 @@
 #ifndef DQSTRING_H
 #define DQSTRING_H
 
+
 const char *pressIcon[50] = {
-    "../myScene/resources/icons/account_press.png",
-    "../myScene/resources/icons/display_press.png",
-    "../myScene/resources/icons/default_applications_press.png",
-    "../myScene/resources/icons/personalization_press.png",
-    "../myScene/resources/icons/network_press.png",
-    "../myScene/resources/icons/sound_press.png",
-    "../myScene/resources/icons/date_time_press.png",
-    "../myScene/resources/icons/power_press.png",
-    "../myScene/resources/icons/mouse_touchpad_press.png",
-    "../myScene/resources/icons/keyboard_press.png",
-    "../myScene/resources/icons/shortcuts_press.png",
-    "../myScene/resources/icons/grub_press.png",
-    "../myScene/resources/icons/system_info_press.png",
-    "../myScene/resources/icons/home_press.png",
-    "../myScene/resources/icons/shutdown_press.png"
+    "../DeepinGraphics/resources/icons/account_press.png",
+    "../DeepinGraphics/resources/icons/display_press.png",
+    "../DeepinGraphics/resources/icons/default_applications_press.png",
+    "../DeepinGraphics/resources/icons/personalization_press.png",
+    "../DeepinGraphics/resources/icons/network_press.png",
+    "../DeepinGraphics/resources/icons/sound_press.png",
+    "../DeepinGraphics/resources/icons/date_time_press.png",
+    "../DeepinGraphics/resources/icons/power_press.png",
+    "../DeepinGraphics/resources/icons/mouse_touchpad_press.png",
+    "../DeepinGraphics/resources/icons/keyboard_press.png",
+    "../DeepinGraphics/resources/icons/shortcuts_press.png",
+    "../DeepinGraphics/resources/icons/grub_press.png",
+    "../DeepinGraphics/resources/icons/system_info_press.png",
+    "../DeepinGraphics/resources/icons/home_press.png",
+    "../DeepinGraphics/resources/icons/shutdown_press.png"
 };
 
 const char *releaseIcon[50] = {
-    "../myScene/resources/icons/account_normal.png",
-    "../myScene/resources/icons/display_normal.png",
-    "../myScene/resources/icons/default_applications_normal.png",
-    "../myScene/resources/icons/personalization_normal.png",
-    "../myScene/resources/icons/network_normal.png",
-    "../myScene/resources/icons/sound_normal.png",
-    "../myScene/resources/icons/date_time_normal.png",
-    "../myScene/resources/icons/power_normal.png",
-    "../myScene/resources/icons/mouse_touchpad_normal.png",
-    "../myScene/resources/icons/keyboard_normal.png",
-    "../myScene/resources/icons/shortcuts_normal.png",
-    "../myScene/resources/icons/grub_normal.png",
-    "../myScene/resources/icons/system_info_normal.png",
-    "../myScene/resources/icons/home_normal.png",
-    "../myScene/resources/icons/shutdown_normal.png"
+    "../DeepinGraphics/resources/icons/account_normal.png",
+    "../DeepinGraphics/resources/icons/display_normal.png",
+    "../DeepinGraphics/resources/icons/default_applications_normal.png",
+    "../DeepinGraphics/resources/icons/personalization_normal.png",
+    "../DeepinGraphics/resources/icons/network_normal.png",
+    "../DeepinGraphics/resources/icons/sound_normal.png",
+    "../DeepinGraphics/resources/icons/date_time_normal.png",
+    "../DeepinGraphics/resources/icons/power_normal.png",
+    "../DeepinGraphics/resources/icons/mouse_touchpad_normal.png",
+    "../DeepinGraphics/resources/icons/keyboard_normal.png",
+    "../DeepinGraphics/resources/icons/shortcuts_normal.png",
+    "../DeepinGraphics/resources/icons/grub_normal.png",
+    "../DeepinGraphics/resources/icons/system_info_normal.png",
+    "../DeepinGraphics/resources/icons/home_normal.png",
+    "../DeepinGraphics/resources/icons/shutdown_normal.png"
 };
 
 const char *hoverIcon[50] = {
-    "../myScene/resources/icons/account_hover.png",
-    "../myScene/resources/icons/display_hover.png",
-    "../myScene/resources/icons/default_applications_hover.png",
-    "../myScene/resources/icons/personalization_hover.png",
-    "../myScene/resources/icons/network_hover.png",
-    "../myScene/resources/icons/sound_hover.png",
-    "../myScene/resources/icons/date_time_hover.png",
-    "../myScene/resources/icons/power_hover.png",
-    "../myScene/resources/icons/mouse_touchpad_hover.png",
-    "../myScene/resources/icons/keyboard_hover.png",
-    "../myScene/resources/icons/shortcuts_hover.png",
-    "../myScene/resources/icons/grub_hover.png",
-    "../myScene/resources/icons/system_info_hover.png",
-    "../myScene/resources/icons/home_hover.png",
-    "../myScene/resources/icons/shutdown_hover.png"
+    "../DeepinGraphics/resources/icons/account_hover.png",
+    "../DeepinGraphics/resources/icons/display_hover.png",
+    "../DeepinGraphics/resources/icons/default_applications_hover.png",
+    "../DeepinGraphics/resources/icons/personalization_hover.png",
+    "../DeepinGraphics/resources/icons/network_hover.png",
+    "../DeepinGraphics/resources/icons/sound_hover.png",
+    "../DeepinGraphics/resources/icons/date_time_hover.png",
+    "../DeepinGraphics/resources/icons/power_hover.png",
+    "../DeepinGraphics/resources/icons/mouse_touchpad_hover.png",
+    "../DeepinGraphics/resources/icons/keyboard_hover.png",
+    "../DeepinGraphics/resources/icons/shortcuts_hover.png",
+    "../DeepinGraphics/resources/icons/grub_hover.png",
+    "../DeepinGraphics/resources/icons/system_info_hover.png",
+    "../DeepinGraphics/resources/icons/home_hover.png",
+    "../DeepinGraphics/resources/icons/shutdown_hover.png"
 
 };
 
